d8e4f2f09a90542fff162d4abfb2e578a3457014
Vishal Raman <rmnvishal@gmail.com>
2020-12-28 18:46:38 +0530

    some changes

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for
 
 app = Flask(__name__)
 
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(debug=True)
+    url_for('static', filename='VotacionesSenado2017.json')
